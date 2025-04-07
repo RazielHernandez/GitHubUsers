94f37c8f58aebf6dbcc4e2dc71eff583474e15f9
RazielHernandez <113457756+RazielHernandez@users.noreply.github.com>
2025-04-06 22:18:51 -0400

    Initial Commit

---
+++ GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Raziel Hernandez on 2025-04-06.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
